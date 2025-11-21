64d6376341eb7d0e3b21db3740efdbacf2be580c
Matheus Felipe <theusf40@hotmail.com>
2025-11-21 16:18:32 -0300

    Fix: modifiquei o endpoint pro /

--- src/main/java/dev/matheusfelipe/eventmaster/PingController.java
+++ src/main/java/dev/matheusfelipe/eventmaster/PingController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
 
 @RestController
-@RequestMapping("/ping")
+@RequestMapping("/")
 public class PingController {
     
     @GetMapping
