afb729b755ad3c97ccf7dc55f0668374d1fb6613
Matheus Felipe <theusf40@hotmail.com>
2026-03-15 11:05:55 -0300

    Added staff to RoleSeeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -16,6 +16,7 @@ class RoleSeeder extends Seeder
             ['name' => 'user'],
             ['name' => 'admin'],
             ['name' => 'organizer'],
+            ['name' => 'staff'],
         ]);
     }
 }
