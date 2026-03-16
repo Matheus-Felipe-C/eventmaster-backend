ec88c1be17e5477bcb5be55ede79aa1e70447ae9
Matheus Felipe <theusf40@hotmail.com>
2026-03-15 21:14:20 -0300

    Testing auto-deploy from Coolify

--- routes/api.php
+++ routes/api.php
@@ -60,4 +60,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('/cart', CartController::class)
         ->only(['index', 'store', 'update', 'destroy'])
         ->parameters(['cart' => 'cartItem']);
+        
 });
